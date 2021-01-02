ad928aa5271a3dfaebbe772cc199ba362c2220c0
Arezou Sohrabvand <200387480@student.georgianc.on.ca>
2021-01-02 16:23:17 -0500

    Upload Quiz application

---
+++ src/views/About.vue
@@ -0,0 +1,202 @@
+<template>
+
+  <div class="pure-g">
+    <div class="pure-u-2-24">
+    </div>
+    
+    <div class="pure-u-20-24">
+    <div class="container">
+      <div class="text-box">
+      <h1>About Baby Yoda Quiz</h1>
+      <h1>About Baby Yoda Quiz</h1>
+      </div>
+      <p id="one" onmouseover="style.color='gold'" onmouseout="style.color='#484848'">Meet the Team</p>
+      </div>
+      
+      <table class="pure-table">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Github</th>
+                <th>Student Number</th>
+                <th>Class Day</th>
+            </tr>
+        </thead>
+        <tbody>
+          <tr v-for="person in team.students" :key="person.name">
+            <td>{{person.name}}</td>
+            <td> <a :href="person.github">{{person.github}}</a></td>
+            <td>{{person.stuNumber}}</td>
+            <td>{{person.classDay}}</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <div id="intro">
+    
+        <input id="btn" @click="showDetails = !showDetails" class="pure-button pure-button-primary" value="Detailed Description">
+       
+        <transition  name="bounce">
+          <p v-if="showDetails">This project is a quiz platform that can be used to create quizzes, 
+                        take quizzes, and see your quiz result from a database. We will do 
+                        our best to make the web application modular so that the quizzes can
+                        be changed easily. We will be able to use forms to add quizzes and 
+                        quiz questions to the database. Main idea: Quiz management platform.
+                        Save quizzes to local storage.Simpler idea: One static quiz with ability
+                        to create user profile and keep track of scores in a database
+          </p>
+        </transition >
+        </div>
+
+    </div>
+
+    <div class="pure-u-2-24">
+    </div>
+</div>
+
+
+</template>
+
+
+ 
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+      team: {},
+      showDetails:true
+    }
+  },
+  // mounted() hook is called when Vue app is mounted
+  mounted() {
+    this.team = {
+        "students": [
+          {"name": "Diana", "github": "https://github.com/DianaAvanesy","stuNumber":200421248,classDay:"Tuesday"},
+          {"name": "Delroy", "github": "https://github.com/delroydennie","stuNumber":200329242,classDay:"Tuesday"},
+          {"name": "Liam", "github": "https://github.com/Rokket14","stuNumber":200427653,classDay:"Tuesday"},
+          {"name": "Arezou", "github": "https://github.com/arezousohrabvand","stuNumber":200387480,classDay:"Monday-Section3"},
+        ]
+      };
+  }
+}
+
+
+</script>
+
+<style>
+.bounce-enter-active{
+  animation:bounce-in .6;
+}
+.bounce-leave-active{
+  animation:bounce-in .6 reverse;
+}
+@keyframes bounce-in{
+  0%{
+    transform:scale(0);
+  }
+  50%{
+    transform:scale(1.5);
+  }
+  100%{
+    transform:scale(1);
+  }
+
+}
+
+#intro{
+  background-color:#f2f2f2;
+  position:fixed;
+  margin-right:500px;
+  padding:20px;
+  margin-top:40px;
+  text-align:justify;
+  border:solid #484848 5px;
+}
+#btn{
+  background-color:#484848;
+  padding:10px  20px;
+  text-align:center;
+  outline:none;
+  cursor:pointer;
+  border:none;
+  border-radius:13px;
+  box-shadow:0 7px #b3b3b3;
+}
+#btn:hover{
+  background-color:#484848;
+}
+#btn:active{
+  background-color:#00334d;
+  box-shadow:0 7px #e6e6e6;
+  transform:translateY(5px);
+}
+.pure-table{
+  border:2px solid #484848;
+}
+thead td{
+  background-color:#E1E7EE;
+}
+th,
+td{
+  border-bottom:1px solid #484848;
+  line-height:1.2;
+  text-align:left;
+  padding:0.4rem;
+}
+th{
+  background:#484848;
+  font-size:0.5rem;
+  color:white;
+  text-tranform:uppercase;
+}
+
+*,
+*:after,
+*:before{
+  box-sizing:border-box;
+}
+body{
+  font-family:"Inter",sans-serif;
+  background-color:#fff;
+}
+
+.text-box{
+  display:flex;
+  align-items:left;
+  justify-content:left;
+  position:relative;
+}
+
+h1{
+  font-size:4vw;
+  font-weight:600;
+  background-color:#484848;
+  display:block;
+  color:#fff;
+  padding:0.5rem;
+  text-align:left;
+   margin-left:15px;
+}
+h1:nth-child(2)
+{
+  background-color:#fff;
+  color:#484848;
+  position:absolute;
+  clip-path:inset(-1% -1% 50% -1%);
+}
+
+#one{
+  font-size:2vm;
+  font-weight:600;
+  text-align:left;
+  margin-left:260px;
+  margin-top:0.1rem;
+  color:#484848;
+  
+}
+
+
+
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,201 @@
+<template>
+  <!--
+    We will be using the Pure CSS framework (https://purecss.io/)
+    Vue is very "greedy" when it comes to DOM manipulation, so a pure
+    CSS framework without and javascript is what we want.
+  -->
+  <div class="pure-g">
+    <div class="pure-u-2-24">
+    </div>
+    
+    <div class="pure-u-20-24">
+      <h1>Test Your JavaScript Knowledge</h1>
+
+      <!-- 
+        This is the main form populated from the API and used to gather user's responses.
+        We need to overwrite the default form handler with our own onSubmit().
+        Otherwise, the form will post in itself and we will lose the state of our internal data structures.
+      -->
+      <form id="quizForm" class="pure-form" @submit.prevent="onSubmit">
+        <fieldset  v-for="question in questions" :key="question._id" :id="question._id">
+          <legend>
+            {{question.query}} 
+            <span v-if="responses[question._id].incorrect" class="error-tag">&#x292B;</span>
+          </legend>
+          <label v-for="choice in question.choices" :key="choice.key" :for="choice._id" class="pure-radio">
+            <input @change="onChange" v-model="responses[question._id].selected" type="radio"
+              :id="choice._id" :name="question._id" :value="choice.key" /> {{choice.description}}
+          </label>
+        </fieldset>
+        <hr/>
+        <p class="buttons">
+          <input class="pure-button pure-button-primary" type="submit" value="Submit">
+        </p>
+      </form>
+
+      <div class="vertical-space"></div>
+
+      <!--
+        This section will be removed from DOM when user makes changes.
+        It will be added to DOM again once they hit Submit to recalculate the results.
+
+        This is a "fake" form, just to reuse the nice CSS classes we defined.
+        It doesn't actually any interactive controls to trigger submission,
+        but we are still going to prevent submission just in case.
+      -->
+      <form v-if="resultsReady" class="pure-form" v-on:submit.prevent="">
+        <fieldset class="summary">
+          <legend>Your Results!</legend>
+          <span class="results results-total" v-if="totalQuestions"> Total questions: {{totalQuestions}} </span>
+          <span class="results results-correct" v-if="correctAnswers"> Correct answers: {{correctAnswers}} </span>
+          <span class="results results-incorrect" v-if="incorrectAnswers"> Inorrect answers: {{incorrectAnswers}} </span>
+        </fieldset>
+      </form>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: 'Home',
+
+  // Initialize reactive data properties
+  // Vue needs these to be happy
+  data() {
+    return {
+      questions: {},
+      responses: {},
+      err: null,
+      api: "http://192.168.0.13:5000/api/v1/questions",
+      totalQuestions: null,
+      correctAnswers: null,
+      incorrectAnswers: null,
+      resultsReady: false,
+    }
+  },
+
+  methods: {
+    // Pull the list of questions from the backend API using axios
+    // and initialize questions and responses objects.
+    // We need a separate response object to keep track of which
+    // option the user selected, which option is the correct one,
+    // and finally whether they answered incorrectly or not.
+    loadData() {
+      this.err = false;
+      axios
+        .get(this.api)
+        .then(res => {
+          this.questions = res.data.data;
+          console.log(this.questions);
+          this.totalQuestions = Object.keys(this.questions).length;
+          this.questions.forEach(question => {
+            this.responses[question._id] = {
+              correct: question.correct,
+              selected: null,
+              incorrect: false
+            }
+          });
+        })
+        .catch(err => {
+          this.err = true;
+        });
+    },
+
+    // Hide results when user makes a different selection
+    // We'll have to recalculate them when they hit submit again
+    onChange() {
+      this.resultsReady = false;
+    },
+
+    // (Re)calculate results (correct and incorrect questions) when user hits Submit
+    onSubmit() {
+      this.correctAnswers = 0;
+      var questionID;
+      for (questionID in this.responses) {
+        if (this.responses[questionID].correct === this.responses[questionID].selected) {
+          this.responses[questionID].incorrect = false
+          this.correctAnswers += 1;
+        } else {
+          this.responses[questionID].incorrect = true
+        }
+      }
+      this.incorrectAnswers = this.totalQuestions - this.correctAnswers;
+
+      this.resultsReady = true;
+    }
+  },
+
+  // mounted() hook is called when Vue app is mounted
+  mounted() {
+    this.loadData()
+  }
+}
+</script>
+
+<style>
+
+h1 {
+  text-align: center;
+}
+
+fieldset {
+  padding-bottom: 10px;
+}
+
+label {
+  padding-left: 10px;
+}
+
+.pure-form {
+  border-style: solid;
+  border-color: lightgray;
+}
+
+.pure-form legend {
+  background-color: gray;
+  padding-left: 10px;
+  font-weight: bold;
+  color: white;
+}
+
+.buttons {
+  text-align: center;
+}
+
+.vertical-space {
+  padding-bottom: 10px;
+}
+
+.error-tag {
+  margin: 0 0.1em;
+  padding: 0.1em 0.3em;
+  color: white;
+  background: orangered;
+}
+
+.results {
+  margin: 0 0.1em;
+  padding: 0.3em 1em;
+}
+
+  .results-total {
+    background: lightskyblue;
+  }
+
+  .results-correct {
+    background: lightgreen;
+  }
+
+  .results-incorrect {
+    background: lightsalmon;
+  }
+
+
+.summary {
+  text-align: center;
+}
+
+</style>
